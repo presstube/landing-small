6df37ad8338bfd054acfd3fd46ac2745d823d95b
James Paterson <j@presstube.com>
2016-05-05 11:14:17 -0400

    blocking in blurb and CTA

--- src/components/blurb.js
+++ src/components/blurb.js
@@ -6,11 +6,32 @@ export default class Blurb extends React.Component {
     return (
       <div
         style={{
-          // width: 300,
-          // height: 300,
-          // backgroundColor: "red"
+          margin: 0,
+          marginTop: 20,
+          // padding: 0,
+          padding: "10px 20px 10px 20px",
+          maxWidth: 500,
+          color: "#eee",
+          backgroundColor: "rgba(0,0,0,0.5)",
+          borderRadius: 4
         }}>
-        BLURB BLURBBLU RBBLURB BLURB BLURB 
+
+        <p>
+          Presstube Studio is a boutique software consultancy. 
+        </p>
+
+        <p>
+          We specialize in prototyping, MVP production, custom tool-building, and bespoke user interfaces.
+        </p>
+
+        <p>
+          Founded in 2000 by James Paterson (watch James' EYEO talk) Presstube has been cracking creative and technical nuts for likes of Apple, Nike, Sony, Burton, Bjork, Sid Lee, and Fiftythree ever since.
+        </p>
+
+        <p>
+          If you have an idea you'd like to bring to life you should get in touch!
+        </p>
+
       </div>
     )
   }
--- src/components/cta.js
+++ src/components/cta.js
@@ -4,8 +4,22 @@ import React from "react"
 export default class CTA extends React.Component {
   render () {
     return (
-      <div>
-        CTA
+      <div 
+        style={{
+          marginTop: 40,
+          // padding: 20,
+        }}>
+        <a
+          href="mailto:j@presstube.com"
+          style={{
+            color: "#eee",
+            padding: 20,
+            // backgroundColor: "rgba(255, 255, 255, 0.5)",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            borderRadius: 4
+          }}>
+            CONTACT
+          </a>
       </div>
     )
   }
